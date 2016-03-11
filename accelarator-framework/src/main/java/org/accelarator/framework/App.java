6848468480c82cdd1185a2551e4dbfc77d206ee2
zhuhuihui <zhuhuihui@gochinatv.com>
2016-03-11 18:37:27 +0800

    初始化

---
+++ accelarator-framework/src/main/java/org/accelarator/framework/App.java
@@ -0,0 +1,13 @@
+package org.accelarator.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
