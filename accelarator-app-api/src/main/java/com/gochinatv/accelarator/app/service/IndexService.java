1b282af6deddd57e548ca68c882ec91f8f60f799
zhuhuihui <zhuhuihui@gochinatv.com>
2016-03-14 17:12:06 +0800

    app init

---
+++ accelarator-app-api/src/main/java/com/gochinatv/accelarator/app/service/IndexService.java
@@ -0,0 +1,14 @@
+package com.gochinatv.accelarator.app.service;
+
+
+
+/**
+ * @作者 zhuhh
+ * @描述    广告套餐关联业务层接口
+ * @创建时间 2016年3月14日 下午12:55:23
+ * @修改时间
+ */
+public interface IndexService{
+   
+
+}
