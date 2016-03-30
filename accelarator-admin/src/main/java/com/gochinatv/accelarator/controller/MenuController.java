9a4698c0fea9b6becb039c9d383c0e7e957235ae
zhuhuihui <zhuhuihui@gochinatv.com>
2016-03-30 18:01:25 +0800

    commit

--- accelarator-admin/src/main/java/com/gochinatv/accelarator/controller/MenuController.java
+++ accelarator-admin/src/main/java/com/gochinatv/accelarator/controller/MenuController.java
@@ -3,14 +3,11 @@ package com.gochinatv.accelarator.controller;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.gochinatv.accelarator.dao.entity.Menu;
 import com.gochinatv.accelarator.framework.web.base.controller.BaseController;
 import com.gochinatv.accelarator.framework.web.base.pagination.PageInfo;
